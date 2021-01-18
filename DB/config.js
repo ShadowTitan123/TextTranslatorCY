ea5f63a706d5514731726e6f1b57c06076853c9a
Adil <mdadilehsan77@gmail.com>
2021-01-18 19:53:18 +0530

    final commit

--- DB/config.js
+++ DB/config.js
@@ -1,21 +1,21 @@
 const mysql = require('mysql')
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'cytranslator'
-})
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'cytranslator'
+// })
 
 
 //For Heroku Server
 
 
-// const connection = mysql.createConnection({
-//   host: 'us-cdbr-east-03.cleardb.com',
-//   user: 'b83390b77425af',
-//   password: 'abaca946',
-//   database: 'heroku_52228eb2a81f0d2'
-// })
+const connection = mysql.createConnection({
+  host: 'us-cdbr-east-03.cleardb.com',
+  user: 'b83390b77425af',
+  password: 'abaca946',
+  database: 'heroku_52228eb2a81f0d2'
+})
 
 
 
